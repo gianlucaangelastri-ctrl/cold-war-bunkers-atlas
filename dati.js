3a64a7a95408e7253feb46cc2b5537bcc023ab03
gianlucaangelastri-ctrl <gianlucaangelastri@gmail.com>
2026-07-04 01:48:16 +0200

    Create dati.js

---
+++ dati.js
@@ -0,0 +1,14 @@
+const sitiMilitari = [
+  {
+    lat: 45.316,
+    lng: 11.666,
+    titolo: "1° ROC Monte Venda",
+    info: "Ex base militare NATO operativa durante la Guerra Fredda."
+  },
+  {
+    lat: 42.245,
+    lng: 12.501,
+    titolo: "Bunker del Monte Soratte",
+    info: "Complesso ipogeo militare situato a Sant’Oreste, Roma. Realizzato durante la Seconda guerra mondiale e successivamente riadattato nel contesto della Guerra Fredda come struttura protetta di comando e rifugio."
+  }
+];
