0f606144039d76db400c08d9f21731190d29090a
gianlucaangelastri-ctrl <gianlucaangelastri@gmail.com>
2026-07-04 01:55:15 +0200

    Update dati.js

--- dati.js
+++ dati.js
@@ -1,14 +1,68 @@
 const sitiMilitari = [
-  {
-    lat: 45.316,
-    lng: 11.666,
-    titolo: "1° ROC Monte Venda",
-    info: "Ex base militare NATO operativa durante la Guerra Fredda."
-  },
-  {
-    lat: 42.245,
-    lng: 12.501,
-    titolo: "Bunker del Monte Soratte",
-    info: "Complesso ipogeo militare situato a Sant’Oreste, Roma. Realizzato durante la Seconda guerra mondiale e successivamente riadattato nel contesto della Guerra Fredda come struttura protetta di comando e rifugio."
-  }
+    { 
+        lat: 42.2456, 
+        lng: 12.5019, 
+        titolo: "Gallerie del Monte Soratte", 
+        info: "Sant'Oreste, Roma" 
+    },
+    { 
+        lat: 42.1288, 
+        lng: 12.7238, 
+        titolo: "DC-75", 
+        info: "Montelibretti, Roma" 
+    },
+    { 
+        lat: 45.5569, 
+        lng: 10.7697, 
+        titolo: "JCOC West Star - Bunker NATO", 
+        info: "Affi, Verona" 
+    },
+    { 
+        lat: 45.5166, 
+        lng: 11.0166, 
+        titolo: "Back Yard - Bunker NATO", 
+        info: "Grezzana, Verona" 
+    },
+    { 
+        lat: 41.2165, 
+        lng: 13.9585, 
+        titolo: "Base NATO PROTO - MARINA", 
+        info: "Mondragone, Caserta" 
+    },
+    { 
+        lat: 42.0160, 
+        lng: 12.3551, 
+        titolo: "COCAN - Bunker NATO - MARINA", 
+        info: "Santarosa, Roma" 
+    },
+    { 
+        lat: 37.2407, 
+        lng: 15.2215, 
+        titolo: "Palombara Presunto Bunker - MARINA", 
+        info: "Augusta, Siracusa" 
+    },
+    { 
+        lat: 41.8315, 
+        lng: 12.5292, 
+        titolo: "COSME Centro Operativo S.M. Esercito", 
+        info: "Forte Appio, Roma - ESERCITO" 
+    },
+    { 
+        lat: 41.7583, 
+        lng: 12.7061, 
+        titolo: "COSMA Centro Operativo S.M. Aereonautica", 
+        info: "Monte Cavo, Roma (ex 2° ROC_Base NATO, poi COSMA con sale di guerra)" 
+    },
+    { 
+        lat: 45.3160, 
+        lng: 11.6660, 
+        titolo: "1° ROC_Base NATO Venda", 
+        info: "Teolo, Padova" 
+    },
+    { 
+        lat: 40.7027, 
+        lng: 17.3364, 
+        titolo: "3° ROC_Base NATO", 
+        info: "Martina Franca, Taranto" 
+    }
 ];
