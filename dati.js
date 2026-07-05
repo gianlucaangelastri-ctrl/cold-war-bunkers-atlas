bfcbfbca508c6977f22e7854f669f906c997846c
gianlucaangelastri-ctrl <gianlucaangelastri@gmail.com>
2026-07-05 20:13:04 +0200

    Update dati.js

--- dati.js
+++ dati.js
@@ -3,17 +3,17 @@ const sitiMilitari = [
         lat: 42.2456, 
         lng: 12.5019, 
         titolo: "Gallerie del Monte Soratte", 
-        info: "Sant'Oreste, Roma" 
+        info: "Sant'Oreste, Roma",
         immagine: "img/soratte.jpg"
     },
     { 
         lat: 42.1288, 
-        lng: 12.7238,
+        lng: 12.7238, 
         titolo: "DC-75", 
-        info: "Montelibretti, Roma"
+        info: "Montelibretti, Roma",
         immagine: "img/dc75.jpg"
     },
-{ 
+    { 
         lat: 45.5569, 
         lng: 10.7697, 
         titolo: "“Site B” West Star - Bunker NATO", 
@@ -24,54 +24,61 @@ const sitiMilitari = [
         lat: 45.5166, 
         lng: 11.0166, 
         titolo: "“Site A” Back Yard - Bunker NATO", 
-        info: "Grezzana, Verona" 
+        info: "Grezzana, Verona",
         immagine: "img/backyard.jpg"
     },
     { 
         lat: 41.2165, 
         lng: 13.9585, 
         titolo: "Base NATO PROTO - MARINA", 
-        info: "Mondragone, Caserta" 
+        info: "Mondragone, Caserta",
         immagine: "img/proto.jpg"
     },
     { 
         lat: 42.0160, 
         lng: 12.3551, 
         titolo: "COCAN - Bunker NATO - MARINA", 
-        info: "Santarosa, Roma"
+        info: "Santarosa, Roma",
         immagine: "img/santarosa.jpg"
     },
     { 
         lat: 37.2407, 
         lng: 15.2215, 
         titolo: "Palombara Presunto Bunker - MARINA", 
-        info: "Augusta, Siracusa" 
+        info: "Augusta, Siracusa"
     },
     { 
         lat: 41.8315, 
         lng: 12.5292, 
         titolo: "COSME Centro Operativo S.M. Esercito", 
-        info: "Forte Appio, Roma - ESERCITO" 
+        info: "Forte Appio, Roma - ESERCITO"
     },
     { 
         lat: 41.7583, 
         lng: 12.7061, 
         titolo: "COSMA Centro Operativo S.M. Aereonautica", 
-        info: "Monte Cavo, Roma (ex 2° ROC_Base NATO, poi COSMA con sale di guerra)" 
+        info: "Monte Cavo, Roma (ex 2° ROC_Base NATO, poi COSMA con sale di guerra)",
         immagine: "img/montecavo.jpg"
     },
     { 
         lat: 45.3160, 
         lng: 11.6660, 
         titolo: "1° ROC_Base NATO Venda - Rupe", 
-        info: "Teolo, Padova" 
+        info: "Teolo, Padova",
         immagine: "img/venda.jpg"
     },
     { 
         lat: 40.7027, 
         lng: 17.3364, 
         titolo: "3° ROC_Base NATO - Sasso", 
-        info: "Martina Franca, Taranto" 
+        info: "Martina Franca, Taranto",
         immagine: "img/martinafranca.jpg"
+    },
+    { 
+        lat: 44.0950, 
+        lng: 9.8130, 
+        titolo: "Centrale Elettrica Protetta Arsenale Spezia", 
+        info: "Acquasanta, La Spezia",
+        immagine: "img/laspezia.jpg"
     }
 ];
