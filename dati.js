04a1044fb3a7872f28dc21bb9d838c34d9920612
gianlucaangelastri-ctrl <gianlucaangelastri@gmail.com>
2026-07-08 11:57:16 +0200

    Update dati.js

--- dati.js
+++ dati.js
@@ -1,84 +1,162 @@
 const sitiMilitari = [
-    { 
-        lat: 42.2456, 
-        lng: 12.5019, 
-        titolo: "Gallerie del Monte Soratte", 
-        info: "Sant'Oreste, Roma",
-        immagine: "img/soratte.jpg"
-    },
-    { 
-        lat: 42.1288, 
-        lng: 12.7238, 
-        titolo: "DC-75", 
-        info: "Montelibretti, Roma",
-        immagine: "img/dc75.jpg"
-    },
-    { 
-        lat: 45.5569, 
-        lng: 10.7697, 
-        titolo: "“Site B” West Star - Bunker NATO", 
-        info: "Affi, Verona",
+    {
+        n: 1,
+        lat: 45.5569,
+        lng: 10.7697,
+        titolo: "West Star",
+        sottotitolo: "Site B",
+        categoria: "C3I | Static War Headquarters - Joint Primary",
+        info: "Monte Moscal, Affi (Verona)",
         immagine: "img/weststar.jpg"
     },
-    { 
-        lat: 45.5166, 
-        lng: 11.0166, 
-        titolo: "“Site A” Back Yard - Bunker NATO", 
-        info: "Grezzana, Verona",
+    {
+        n: 2,
+        lat: 45.5166,
+        lng: 11.0166,
+        titolo: "Back Yard",
+        sottotitolo: "Site A",
+        categoria: "C3I | Static War Headquarters - Joint Alternate",
+        info: "Grezzana (Verona)",
         immagine: "img/backyard.jpg"
     },
-    { 
-        lat: 41.2165, 
-        lng: 13.9585, 
-        titolo: "Base NATO PROTO - MARINA", 
-        info: "Mondragone, Caserta",
+    {
+        n: 3,
+        lat: 41.2165,
+        lng: 13.9585,
+        titolo: "Base PROTO",
+        sottotitolo: "",
+        categoria: "C3I | Static War Headquarters - Joint",
+        info: "Monte Massico, Mondragone (Caserta)",
         immagine: "img/proto.jpg"
     },
-    { 
-        lat: 42.0160, 
-        lng: 12.3551, 
-        titolo: "COCAN - Bunker NATO - MARINA", 
-        info: "Santarosa, Roma",
-        immagine: "img/santarosa.jpg"
+    {
+        n: 4,
+        lat: 41.9187,
+        lng: 12.4085,
+        titolo: "C.O.P.I.",
+        sottotitolo: "Centro Operativo Interforze",
+        categoria: "C3I | Static War Headquarters - Joint",
+        info: "Forte Braschi, Roma",
+        immagine: ""
     },
-    { 
-        lat: 37.2407, 
-        lng: 15.2215, 
-        titolo: "Bunker - MARINA, sede alternata di Santarosa", 
-        info: "Melilli, Siracusa"
-    },
-    { 
-        lat: 41.8315, 
-        lng: 12.5292, 
-        titolo: "COSME Centro Operativo S.M. Esercito", 
-        info: "Forte Appio, Roma - ESERCITO"
-    },
-    { 
-        lat: 41.7583, 
-        lng: 12.7061, 
-        titolo: "COSMA Centro Operativo S.M. Aereonautica", 
-        info: "Monte Cavo, Roma (ex 2° ROC_Base NATO, poi COSMA con sale di guerra)",
-        immagine: "img/montecavo.jpg"
-    },
-    { 
-        lat: 45.3160, 
-        lng: 11.6660, 
-        titolo: "1° ROC_Base NATO Venda - Rupe", 
-        info: "Teolo, Padova",
+    {
+        n: 5,
+        lat: 45.3160,
+        lng: 11.6660,
+        titolo: "1° R.O.C. Monte Venda",
+        sottotitolo: "Rupe",
+        categoria: "C3I | Static War Headquarters - Air Defence",
+        info: "Monte Venda, Teolo (Padova)",
         immagine: "img/venda.jpg"
     },
-    { 
-        lat: 40.7027, 
-        lng: 17.3364, 
-        titolo: "3° ROC_Base NATO - Sasso", 
-        info: "Martina Franca, Taranto",
+    {
+        n: 6,
+        lat: 41.7583,
+        lng: 12.7061,
+        titolo: "2° R.O.C. / C.O.S.M.A. Monte Cavo",
+        sottotitolo: "Centro Operativo S.M. Aeronautica",
+        categoria: "C3I | Static War Headquarters - Air Defence",
+        info: "Monte Cavo, Rocca di Papa (Roma)",
+        immagine: "img/montecavo.jpg"
+    },
+    {
+        n: 7,
+        lat: 40.7027,
+        lng: 17.3364,
+        titolo: "3° R.O.C./S.O.C.",
+        sottotitolo: "Sasso",
+        categoria: "C3I | Static War Headquarters - Air Defence",
+        info: "Martina Franca (Taranto)",
         immagine: "img/martinafranca.jpg"
     },
-    { 
-        lat: 44.0950, 
-        lng: 9.8130, 
-        titolo: "Centrale Elettrica Protetta Arsenale Spezia", 
-        info: "Acquasanta, La Spezia",
+    {
+        n: 8,
+        lat: 42.0160,
+        lng: 12.3551,
+        titolo: "Comando Protetto Santa Rosa",
+        sottotitolo: "COMEDCENT / Quartier Generale Marina",
+        categoria: "C3I | Static War Headquarters - Naval Primary",
+        info: "Comprensorio militare di Santa Rosa, Roma",
+        immagine: "img/santarosa.jpg"
+    },
+    {
+        n: 9,
+        lat: 37.2407,
+        lng: 15.2215,
+        titolo: "Comando Protetto Palombara",
+        sottotitolo: "",
+        categoria: "C3I | Static War Headquarters - Naval Alternate",
+        info: "Località Palombara, Melilli (Siracusa)",
+        immagine: "img/palombara.jpg"
+    },
+    {
+        n: 10,
+        lat: 46.1380,
+        lng: 13.1230,
+        titolo: "P.C. Main",
+        sottotitolo: "Posto Comando Main",
+        categoria: "C3I | Static War Headquarters - Army Primary",
+        info: "Santa Margherita di Moruzzo (Udine)",
+        immagine: "img/pcmain.jpg"
+    },
+    {
+        n: 11,
+        lat: 46.1325,
+        lng: 12.8637,
+        titolo: "P.C. Rear",
+        sottotitolo: "Posto Comando Rear",
+        categoria: "C3I | Static War Headquarters - Army Alternate",
+        info: "Tauriano, Spilimbergo (Pordenone)",
+        immagine: "img/pcrear.jpg"
+    },
+    {
+        n: 12,
+        lat: 41.8315,
+        lng: 12.5292,
+        titolo: "C.O.S.M.E.",
+        sottotitolo: "Centro Operativo Stato Maggiore Esercito",
+        categoria: "C3I | Static War Headquarters - Army Primary",
+        info: "Forte Appio, Roma",
+        immagine: ""
+    },
+    {
+        n: 13,
+        lat: 42.2456,
+        lng: 12.5019,
+        titolo: "Bunker del Monte Soratte",
+        sottotitolo: "",
+        categoria: "Civil Defence / Continuity of Government | Government Continuity Shelter",
+        info: "Monte Soratte, Sant’Oreste (RM)",
+        immagine: "img/soratte.jpg"
+    },
+    {
+        n: 14,
+        lat: 42.1288,
+        lng: 12.7238,
+        titolo: "DC-75",
+        sottotitolo: "",
+        categoria: "Civil Defence / Continuity of Government | Government Continuity Shelter",
+        info: "Montelibretti (Roma)",
+        immagine: "img/dc75.jpg"
+    },
+    {
+        n: 15,
+        lat: 44.0950,
+        lng: 9.8130,
+        titolo: "Centrale Elettrica Protetta dell’Acquasanta",
+        sottotitolo: "Arsenale Militare Marittimo",
+        categoria: "Protected logistical infrastructure | Energy",
+        info: "Arsenale Militare Marittimo, Acquasanta (La Spezia)",
         immagine: "img/laspezia.jpg"
+    },
+    {
+        n: 16,
+        lat: 39.1975,
+        lng: 9.1368,
+        titolo: "POL NATO Sant’Elia",
+        sottotitolo: "",
+        categoria: ""Protected logistical infrastructure | Fuel",
+        info: "Sant’Elia, Cagliari",
+        immagine: ""
     }
 ];
